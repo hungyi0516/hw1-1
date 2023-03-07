fbcb5b3fb95e80a6456ac1a373c24b885591e1de
hungyi0516 <125360244+hungyi0516@users.noreply.github.com>
2023-03-08 03:19:01 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,44 +1,57 @@
 #include "mbed.h"
 
-DigitalOut led_time(LED3);
-DigitalOut led1(LED1);
-DigitalOut led2(LED2);
-DigitalIn button(BUTTON1);
+DigitalOut led(LED1);
 
-Thread led_thread;
+int main(){
+    for (int i = 0; i < 1; i++) {
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 1; ThisThread::sleep_for(500ms);
+        led = 1; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 1; ThisThread::sleep_for(500ms);
 
-// to calculate time
-void led_blink() {
-    while (1) {
-        led_time = !led_time;
-        ThisThread::sleep_for(3s);
+        led = 1; ThisThread::sleep_for(500ms);
+        led = 1; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 1; ThisThread::sleep_for(500ms);
+        led = 1; ThisThread::sleep_for(500ms);
+
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 1; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 1; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+
+        led = 1; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+
+        led = 1; ThisThread::sleep_for(500ms);
+        led = 1; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 1; ThisThread::sleep_for(500ms);
+
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 1; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 1; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 0; ThisThread::sleep_for(500ms);
+        led = 1; ThisThread::sleep_for(500ms);
+
     }
-}
-int main() {
-    int count = 0;
-
-    led_thread.start(led_blink);
-
-    while (1) {
-            led1 = 0;
-            led2 = 0;
-            count = 0;
-            // to see the count in the 3s
-            for (int i = 0; i < 30; i++) {
-                if (button == 0) count++;
-                else count = count;
-                wait_us(100000);  
-            } 
-            if (count > 3) {
-                led1 = 1; 
-                led2 = 0;
-                printf("1\n\r");
-            }
-            else {
-                led2 = 1;
-                led1 = 0;
-                printf("0\n\r");
-            }  
-    }
-    
+    led = 0;
 }
